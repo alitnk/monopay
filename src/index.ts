e8e7a9a7ecca25ad6cc77a8a1a38a499744b1791
Ali <steelsheetflyer@gmail.com>
2021-10-06 11:45:15 +0330

    Added a driver-agnostic API (experimental)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+import { Driver } from './driver';
+import * as zarinpal from './drivers/zarinpal';
+
+type DriverName = 'zarinpal';
+
+export const polypay = (driver: DriverName) => {
+  const map: Record<DriverName, Driver> = {
+    zarinpal: {
+      purchase: zarinpal.purchase,
+      verify: zarinpal.verify,
+      verifyManually: zarinpal.verifyManually,
+    },
+  };
+  return map[driver];
+};
+
 export * from './driver';
 export * from './receipt';
 export * from './invoice';
