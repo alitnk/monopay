07bae09574a4a54987206a67e8631a9664eba573
alitnk <alirezazamani2922@gmail.com>
2021-10-07 19:16:36 +0330

    introduced getPaymentDriver

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import * as drivers from './drivers';
+import { Invoice } from './invoice';
+import { Receipt } from './receipt';
+import { Requestish } from './utils';
 
 type DriverName = 'zarinpal' | 'zibal';
+type Driver = {
+  purchase(invoice: Invoice): Promise<string>;
+  verify(object: any, req: Requestish): Promise<Receipt>;
+  verifyManually(object: any): Promise<Receipt>;
+};
 
-export const polypay = (driver: DriverName) => {
+/**
+ * NOTE: You do not have type safety for driver-specific options with this method.
+ * This method is used for when you want to let user decide which driver to use.
+ * If you solely want to use one driver, import the driver explicitly.
+ *
+ * @param driver Enter a driver name (e.g. zarinpal)
+ * @returns A driver with `purchase` and `verify` on it
+ */
+export const getPaymentDriver = (driver: DriverName): Driver => {
   return drivers[driver];
 };
 
