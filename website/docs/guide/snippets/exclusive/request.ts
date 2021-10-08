c13cd26e429b170f5f9f5ad1c7015d9c13fd6115
Ali <steelsheetflyer@gmail.com>
2021-10-08 16:46:22 +0330

    Removed website from the main branch

--- website/docs/guide/snippets/exclusive/request.ts
+++
@@ -1,8 +0,0 @@
-import { zarinpal } from 'polypay.js';
-
-const paymentUrl = await zarinpal.purchase({
-  amount: 50000, // in Toman
-  merchantId: 'some-id-you-got-from-the-service',
-  callbackUrl: 'https://my-site.com/callback',
-  description: 'Description about the purchase',
-});
