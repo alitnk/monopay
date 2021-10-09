9b6663dab44eae8423e58fc5d18a282ae6107f20
Ali <steelsheetflyer@gmail.com>
2021-10-09 15:54:24 +0330

    Exceptions change for tests to pass

--- src/exception.ts
+++ src/exception.ts
@@ -1,11 +1,26 @@
-export class PolypayException extends Error {}
+export class PolypayException extends Error {
+  constructor(message?: string) {
+    super(message);
+    Object.setPrototypeOf(this, PolypayException.prototype);
+  }
+}
 
 /**
  * Error in the paying stage
  */
-export class PaymentException extends PolypayException {}
+export class PaymentException extends PolypayException {
+  constructor(message?: string) {
+    super(message);
+    Object.setPrototypeOf(this, PaymentException.prototype);
+  }
+}
 
 /**
  * Error in the verification stage
  */
-export class VerificationException extends PolypayException {}
+export class VerificationException extends PolypayException {
+  constructor(message?: string) {
+    super(message);
+    Object.setPrototypeOf(this, VerificationException.prototype);
+  }
+}
