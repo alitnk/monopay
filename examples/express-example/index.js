9b9e47c88ac61d7adb5eaa14afee1275e6bce714
alitnk <alirezazamani2922@gmail.com>
2021-10-15 09:03:02 +0330

    Updated the express example

--- examples/express-example/index.js
+++ examples/express-example/index.js
@@ -3,9 +3,7 @@ const express = require('express')
 const { getPaymentDriver } = require('polypay')
 
 const app = express()
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
 const port = 3000
 
 /** @type {import('polypay').ConfigObject} */
@@ -14,7 +12,7 @@ const polypayConfiguration = {
         merchantId: 'your-merchant-id',
         sandbox: true,
     },
-    zibal: {
+    zarinpal: {
         merchantId: 'your-merchant-id',
         sandbox: true,
     },
@@ -23,48 +21,64 @@ const polypayConfiguration = {
         terminalId: 'your-terminal-id',
         terminalKey: 'your-terminal-key'
     },
+    payir: {
+        apiKey: 'your-api-key',
+        sandbox: true,
+    },
+    nextpay: {
+        apiKey: 'your-api-key',
+    }
 }
+
+/** @type {import('polypay').DriverName} */
+const chosenDriver = 'nextpay'
 
 /**
  * The purchase route that will redirect the user to the payment gateway
  */
 app.get('/purchase', async (req, res) => {
-    const driver = getPaymentDriver('zibal', polypayConfiguration.zibal);
+    try {
+        const driver = getPaymentDriver(chosenDriver, polypayConfiguration[chosenDriver]);
 
-    const paymentInfo = await driver.requestPayment({
-        amount: 20000,
-        callbackUrl: process.env.APP_URL + '/callback',
-    })
+        const paymentInfo = await driver.requestPayment({
+            amount: 20000,
+            callbackUrl: process.env.APP_URL + '/callback',
+        })
 
-    // Store the payment info in a database //
+        // Store the payment info in a database //
 
-    res.send(`<html>
+        res.send(`<html>
         <body>
             <h1> We're redirecting you to the payment gateway... </h1>
             <script>${paymentInfo.getScript()}</script>
         </body>
         </html>`)
-
+    } catch (e) {
+        console.log(e.message)
+    }
 })
 
 /**
  * The callback URL that was given to `requestPayment` 
  */
 app.all('/callback', async (req, res) => {
-    const driver = getPaymentDriver('zibal', polypayConfiguration.zibal)
+    try {
+        const driver = getPaymentDriver(chosenDriver, polypayConfiguration[chosenDriver])
 
-    // Get the payment info from database //
+        // Get the payment info from database //
 
-    const receipt = await driver.verifyPayment({
-        amount: 2000, // from database
-        referenceId: 1234 // from database
-    }, { ...req.query, ...req.body }); // support both GET and POST
+        const receipt = await driver.verifyPayment({
+            amount: 2000, // from database
+            referenceId: 1234 // from database
+        }, { ...req.query, ...req.body }); // support both GET and POST
 
-    res.json({
-        referenceId: receipt.referenceId,
-        success: true,
-    })
-
+        res.json({
+            referenceId: receipt.referenceId, // Is probably null if you're using sandbox
+            success: true,
+        })
+    } catch (e) {
+        console.log(e.message)
+    }
 })
 
 app.listen(port, () => {
