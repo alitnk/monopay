75b620a89e529c2a30489ecd33fb858c63dc58e9
Keivan <keyvan0082@gmail.com>
2022-09-06 19:52:19 +0430

    Add basic `Pasargad` types

--- src/drivers/pasargad/api.ts
+++ src/drivers/pasargad/api.ts
@@ -1,4 +1,95 @@
 import * as t from 'io-ts';
+import { BaseReceipt, LinksObject, tBaseRequestOptions, tBaseVerifyOptions } from '../../types';
+
+export const links: LinksObject = {
+  default: {
+    REQUEST: 'https://pep.shaparak.ir/Api/v1/Payment/GetToken',
+    VERIFICATION: 'https://pep.shaparak.ir/Api/v1/Payment/VerifyPayment',
+    PAYMENT: 'https://pep.shaparak.ir/payment.aspx',
+  },
+};
+
+export interface CallbackParams {
+  invoiceNumber: string;
+  invoiceDate: string;
+  amount: number;
+}
+
+export interface VerifyPaymentReq {
+  /**
+   * شماره فاکتور که برای هر خرید باید متفاوت باشد
+   */
+  InvoiceNumber: string;
+  /**
+   * تاریخ فاکتور خرید به فرمت دلخواه.
+   * لازم به ذکر است که تاریخ باید به گونه ای باشد که با شماره فاکتور یک شناسه اختصاصی ساخته
+   * تا همیشه بتوان از آن به عنوان شناسه یکتای خرید استفاده کرد
+   */
+  InvoiceDate: string;
+  /**
+   * شماره شناسایی ترمینال
+   */
+  TerminalCode: string;
+  /**
+   * شماره شناسایی پذیرنده
+   */
+  MerchantCode: string;
+  /**
+   * مبلغ فاکتور
+   */
+  Amount: number;
+  /**
+   * زمان ارسال درخواست
+   * @description فرمت: `YYYY/MM/DD HH:MM:SS`
+   * @example 2019/01/27 17:57:06
+   */
+  Timestamp: string;
+}
+
+export interface VerifyPaymentRes {
+  /**
+   * نشان دهنده موفقیت آمیز بودن و یا شکست عملیات
+   * @example true
+   */
+  IsSuccess: boolean;
+  /**
+   * پیام بانک
+   * @example عملیات با موفقیت انجام شد
+   */
+  Message: string;
+  /**
+   * شمارت کارت ماسک شده
+   * @example 5022-29**-****-2328
+   */
+  MaskedCardNumber: string;
+  /**
+   * شماره کارت هش شده
+   * @example 2DDB1E270C5986...
+   */
+  HashedCardNumber: string;
+  /**
+   * کد پیگیری شاپرک
+   * @example 100200300400500
+   */
+  ShaparakRefNumber: string;
+}
+
+export const tRequestOptions = t.intersection([
+  t.type({
+    certificate_file: t.string,
+    invoiceNumber: t.string,
+    invoiceDate: t.string,
+  }),
+  t.partial({
+    mobile: t.string,
+    email: t.string,
+    name: t.string,
+    PIDN: t.string,
+  }),
+  tBaseRequestOptions,
+]);
+
+export type RequestOptions = t.TypeOf<typeof tRequestOptions>;
 
 export const tConfig = t.interface({
   merchantId: t.string,
@@ -6,3 +97,9 @@ export const tConfig = t.interface({
 });
 
 export type Config = t.TypeOf<typeof tConfig>;
+
+export const tVerifyOptions = t.intersection([t.interface({}), tBaseVerifyOptions]);
+
+export type VerifyOptions = t.TypeOf<typeof tVerifyOptions>;
+
+export type Receipt = BaseReceipt<VerifyPaymentRes>;
--- src/drivers/pasargad/index.ts
+++ src/drivers/pasargad/index.ts
@@ -1,1 +1,40 @@
-export class Pasargad {}
+import axios from 'axios';
+import { Driver } from 'driver';
+import { LinksObject } from 'types';
+import * as API from './api';
+export class Pasargad extends Driver<API.Config> {
+  constructor(config: API.Config) {
+    super(config, API.tConfig);
+  }
+  protected links: LinksObject = API.links;
+  requestPayment = async (options: API.RequestOptions) => {
+    return this.makeRequestInfo(12, 'GET', this.getLinks().PAYMENT, { n: '' });
+  };
+  verifyPayment = async (_options: API.VerifyOptions, params: API.CallbackParams): Promise<API.Receipt> => {
+    const { amount, invoiceDate, invoiceNumber } = params;
+    const response = await axios.post<API.VerifyPaymentReq, { data: API.VerifyPaymentRes }>(
+      this.getLinks().VERIFICATION,
+      {
+        Amount: amount,
+        InvoiceDate: invoiceDate,
+        InvoiceNumber: invoiceNumber,
+        Timestamp: this.getCurrentTimestamp(),
+        TerminalCode: this.config.terminalId,
+        MerchantCode: this.config.merchantId,
+      },
+      {
+        headers: {},
+      },
+    );
+    return {
+      raw: response.data,
+      transactionId: 'problem',
+      cardPan: response.data.MaskedCardNumber,
+    };
+  };
+
+  private getCurrentTimestamp = (): string => {
+    const currentDateISO = new Date().toISOString();
+    return currentDateISO.replace(/-/g, '/').replace('T', ' ').replace('Z', '').split('.')[0];
+  };
+}
