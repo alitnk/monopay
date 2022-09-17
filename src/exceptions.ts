bda6c7cfe588d0549f838c1988df662c7fbce8a0
Keivan <keyvan0082@gmail.com>
2022-09-17 12:44:55 +0430

    Remove old error handling classes

--- src/exceptions.ts
+++ src/exceptions.ts
@@ -3,6 +3,7 @@ type MonoPayErrorConfig = {
   isSafeToDisplay: boolean;
   message?: string;
 };
+
 export abstract class MonopayError extends Error {
   /**
    * Determines whether the error was thrown by the IPG or by the application itself
@@ -16,45 +17,6 @@ export abstract class MonopayError extends Error {
     super(options.message);
     this.isIPGError = options.isIPGError;
     this.isSafeToDisplay = options.isSafeToDisplay;
-  }
-}
-
-export class BasePaymentException extends Error {
-  constructor(message?: string) {
-    super(message);
-    Object.setPrototypeOf(this, BasePaymentException.prototype);
-  }
-}
-
-/**
- * Error in the requesting stage
- */
-export class RequestException extends BasePaymentException {
-  constructor(message?: string) {
-    super(message);
-    Object.setPrototypeOf(this, RequestException.prototype);
-  }
-}
-
-/**
- * Error in the paying stage
- *
- * You can show this error message to your end user
- */
-export class PaymentException extends BasePaymentException {
-  constructor(message?: string) {
-    super(message);
-    Object.setPrototypeOf(this, PaymentException.prototype);
-  }
-}
-
-/**
- * Error in the verification stage
- */
-export class VerificationException extends BasePaymentException {
-  constructor(message?: string) {
-    super(message);
-    Object.setPrototypeOf(this, VerificationException.prototype);
   }
 }
 
