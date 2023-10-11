daef165de0edd1a74313a8015739958e49041ef6
Alireza Zamani <alirezazamani2922@gmail.com>
2023-10-11 22:53:04 +0330

    fix: tests failing

--- packages/monopay/src/driver.spec.ts
+++ packages/monopay/src/driver.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { BadConfigError } from './exceptions';
 import { z } from 'zod';
 import { defineDriver } from './driver';
@@ -40,17 +41,20 @@ describe('Driver', () => {
     expect(paymentInfo.getScript()).toContain('.submit()');
   });
   it('Throws badConfigError when wrong config is passed', () => {
-    expect(() => createTestDriver({ test: '' })).toThrow(BadConfigError);
+    // error is expected here
+    // @ts-expect-error
+    expect(() => createTestDriver({})).toThrow(BadConfigError);
   });
   it('Throws badConfigError when wrong request params are passed', async () => {
     const driver = createTestDriver({ test: 'test' });
     await expect(
       async () =>
+        // error is expected here
+        // @ts-expect-error
         await driver.request({
           amount: 1000,
           callbackUrl: 'https://callback.url/',
           description: 'testin',
-          test: '',
         }),
     ).rejects.toThrow(BadConfigError);
   });
@@ -58,13 +62,13 @@ describe('Driver', () => {
     const driver = createTestDriver({ test: 'test' });
     await expect(
       async () =>
-        await driver.verify(
-          {
-            amount: 1000,
-            test: '',
-          },
-          {},
-        ),
+        // error is expected here
+        // @ts-expect-error
+        await driver.verify({
+          amount: 1000,
+          callbackUrl: 'https://callback.url/',
+          description: 'testin',
+        }),
     ).rejects.toThrow(BadConfigError);
   });
 });
