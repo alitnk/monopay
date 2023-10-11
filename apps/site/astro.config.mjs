b02f69ee45157a544554ef72fa15c81278a81b56
Alireza Zamani <alirezazamani2922@gmail.com>
2023-10-11 22:05:31 +0330

    fix: update astro's site url

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://monopay.js.org',
+	base: '/',
 	redirects: {
 		'/': '/en'
 	},
